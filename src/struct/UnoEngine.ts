0bab40de3811ff733ab6940ca23839fc2db79be8
Dragonizedpizza <dragon@dragonizedpizza.com>
2023-06-05 03:28:15 +0530

    Engine updates, assets

--- src/struct/UnoEngine.ts
+++ src/struct/UnoEngine.ts
@@ -1,14 +1,11 @@
 import { EventEmitter } from "node:events";
 import { generateUnoDeck } from "../types/Constants.js";
-import LodashPkg from "lodash";
 import type { Uno } from "#types";
-
-const { isEqual } = LodashPkg;
 
 export class UnoEngine<Names extends string[]> extends EventEmitter {
 	public hands = {} as Record<Names[number], Uno.Cards>;
 	public deck = generateUnoDeck();
-	 public names: Names;
+	public names: Names;
 	public usedCards: Uno.Cards = [];
 	public currentChance: Names[number];
 	public currentCard!: Uno.Card;
@@ -44,7 +41,10 @@ export class UnoEngine<Names extends string[]> extends EventEmitter {
 
 	public playCard(card: Uno.Card) {
 		if (!this.isPlayable(card)) throw new Error("This card is not playable!");
-		this.hands[this.currentChance].splice(this.hands[this.currentChance].findIndex((c) => card.color === c.color && card.action === c.action), 1);
+		this.hands[this.currentChance].splice(
+			this.hands[this.currentChance].findIndex((c) => card.color === c.color && card.action === c.action),
+			1,
+		);
 
 		let skip = false;
 
@@ -59,19 +59,18 @@ export class UnoEngine<Names extends string[]> extends EventEmitter {
 				skip = true;
 			}
 		} else if (card.action === "DRAW_TWO") {
-				const next = this.getNextChance();
-				this.emit("drawn", this.currentChance, next, 2);
+			const next = this.getNextChance();
+			this.emit("drawn", this.currentChance, next, 2);
 
-				this.drawCard(next, 2);
-				skip = true;
-			} else if (card.action === "SKIP") {
-				this.emit("skipped", this.currentChance, this.getNextChance());
-				skip = true;
-			}
-		else if (card.action === "REVERSE") {
-				this.emit("reversed", this.currentChance, this.getNextChance());
-				this.names.reverse();
-			};
+			this.drawCard(next, 2);
+			skip = true;
+		} else if (card.action === "SKIP") {
+			this.emit("skipped", this.currentChance, this.getNextChance());
+			skip = true;
+		} else if (card.action === "REVERSE") {
+			this.emit("reversed", this.currentChance, this.getNextChance());
+			this.names.reverse();
+		}
 
 		this.currentCard = card;
 		this.nextChance(skip);
@@ -87,9 +86,15 @@ export class UnoEngine<Names extends string[]> extends EventEmitter {
 	}
 
 	public nextChance(skip: boolean = false) {
-		if (this.hands[this.currentChance].length === 0) return this.emit("win", this.currentChance);
 		this.currentChance = this.getNextChance(skip);
 
-		this.emit("nextChance", this.currentChance);
+		if (this.hands[this.currentChance].length === 0) {
+			delete this.hands[this.currentChance];
+			this.names.splice(this.names.indexOf(this.currentChance), 1);
+
+			return this.emit("win", this.currentChance);
+		}
+
+		if (this.names.length > 1) this.emit("nextChance", this.currentChance);
 	}
 }
